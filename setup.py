0739f5b2b64e198c3ba9dbf5e84a7d3990aa8c29
Stephen Bailey <stkbailey@gmail.com>
2020-04-08 06:19:42 -0400

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,19 @@
 #!/usr/bin/env python
-from setuptools import setup
+import setuptools
 
-setup(
+setuptools.setup(
     name="tap-clockify",
     version="0.1.0",
     description="Singer.io tap for extracting data",
-    author="Stitch",
+    author="Stephen Bailey",
     url="http://singer.io",
     classifiers=["Programming Language :: Python :: 3 :: Only"],
     py_modules=["tap_clockify"],
-    install_requires=[
-        "singer-python>=5.0.12",
-        "requests",
-    ],
     entry_points="""
         [console_scripts]
         tap-clockify=tap_clockify:main
     """,
-    packages=["tap_clockify"],
+    packages=setuptools.find_packages(),
     package_data = {
         "schemas": ["tap_clockify/schemas/*.json"]
     },
