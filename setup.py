f335e4cb28fafbf4b09e61bf6d7c080249b767dd
Stephen Bailey <stkbailey@gmail.com>
2020-04-26 13:48:40 -0400

    Update config example and setup

--- setup.py
+++ setup.py
@@ -2,20 +2,20 @@
 import setuptools
 
 setuptools.setup(
-    name="tap-clockify",
-    version="0.1.0",
-    description="Singer.io tap for extracting data",
-    author="Stephen Bailey",
-    url="http://singer.io",
-    classifiers=["Programming Language :: Python :: 3 :: Only"],
-    py_modules=["tap_clockify"],
-    entry_points="""
-        [console_scripts]
-        tap-clockify=tap_clockify:main
-    """,
-    packages=setuptools.find_packages(),
+    name        = "tap-clockify",
+    version     = "0.1.0",
+    description = "Singer.io tap for extracting data",
+    author      = "Stephen Bailey",
+    url         = "http://singer.io",
+    classifiers = ["Programming Language :: Python :: 3 :: Only"],
+    packages    = setuptools.find_packages(),
+    py_modules  = ["tap_clockify"],
     package_data = {
         "schemas": ["tap_clockify/schemas/*.json"]
     },
-    include_package_data=True,
+    entry_points = """
+        [console_scripts]
+        tap-clockify=tap_clockify:main
+    """,
+    include_package_data = True,
 )
