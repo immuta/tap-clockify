ed732d731d467317ef6400a1a9caad091e96e229
Stephen Bailey <stkbailey@gmail.com>
2021-08-14 07:34:07 -0400

    Convert streams from jsonschema to properties

---
+++ tap_clockify/schemas/__init__.py
@@ -0,0 +1,7 @@
+from tap_clockify.schemas.clients import schema as clients
+from tap_clockify.schemas.projects import schema as projects
+from tap_clockify.schemas.tags import schema as tags
+from tap_clockify.schemas.tasks import schema as tasks
+from tap_clockify.schemas.time_entries import schema as time_entries
+from tap_clockify.schemas.users import schema as users
+from tap_clockify.schemas.workspaces import schema as workspaces
---
+++ tap_clockify/schemas/workspaces.py
@@ -0,0 +1,41 @@
+from singer_sdk import typing as th
+
+schema = th.PropertiesList(
+    th.Property("id", th.StringType),
+    th.Property("name", th.StringType),
+    th.Property("imageUrl", th.StringType),
+    th.Property("hourlyRate", th.ObjectType),
+    th.Property(
+        "workspaceSettings",
+        th.ObjectType(
+            th.Property("adminOnlyPages", th.ArrayType(th.StringType)),
+            th.Property("automaticLock", th.ObjectType),
+            th.Property("canSeeTimeSheet", th.BooleanType),
+            th.Property("canSeeTimeTracker", th.BooleanType),
+            th.Property("defaultBillableProjects", th.BooleanType),
+            th.Property("forceDescription", th.BooleanType),
+            th.Property("forceProjects", th.BooleanType),
+            th.Property("forceTags", th.BooleanType),
+            th.Property("forceTasks", th.BooleanType),
+            th.Property("lockTimeEntries", th.StringType),
+            th.Property("onlyAdminsCreateProject", th.BooleanType),
+            th.Property("onlyAdminsCreateTag", th.BooleanType),
+            th.Property("onlyAdminsSeeAllTimeEntries", th.BooleanType),
+            th.Property("onlyAdminsSeeBillableRates", th.BooleanType),
+            th.Property("onlyAdminsSeeDashboard", th.BooleanType),
+            th.Property("onlyAdminsSeePublicProjectsEntries", th.BooleanType),
+            th.Property("projectFavorites", th.BooleanType),
+            th.Property("projectGroupingLabel", th.StringType),
+            th.Property("projectPickerSpecialFilter", th.BooleanType),
+            th.Property(
+                "round",
+                th.ObjectType(
+                    th.Property("minutes", th.StringType),
+                    th.Property("round", th.StringType),
+                ),
+            ),
+            th.Property("timeRoundingInReports", th.BooleanType),
+            th.Property("trackTimeDownToSecond", th.BooleanType),
+        ),
+    ),
+).to_dict()
