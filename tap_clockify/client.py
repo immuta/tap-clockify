6e0e9b12083f30c6e36407eca4d63ef8432a9f0a
Stephen Bailey <stkbailey@gmail.com>
2021-11-29 15:56:16 -0500

    Change authentication to use APIKeyAuthenticator

--- tap_clockify/client.py
+++ tap_clockify/client.py
@@ -4,6 +4,7 @@ import requests
 from pathlib import Path
 from typing import Any, Dict, Optional, Union, List, Iterable
 
+from singer_sdk.authenticators import APIKeyAuthenticator
 from singer_sdk.helpers.jsonpath import extract_jsonpath
 from singer_sdk.streams import RESTStream
 
@@ -22,15 +23,14 @@ class ClockifyStream(RESTStream):
         return f'https://api.clockify.me/api/v1/workspaces/{self.config["workspace"]}'
 
     @property
-    def http_headers(self) -> dict:
-        """Return the http headers needed."""
-        headers = {
-            "Content-Type": "application/json",
-            "x-api-key": self.config["api_key"],
-        }
-        if "user_agent" in self.config:
-            headers["User-Agent"] = self.config.get("user_agent")
-        return headers
+    def authenticator(self) -> APIKeyAuthenticator:
+        return APIKeyAuthenticator.create_for_stream(
+            self,
+            key="x-api-key",
+            value=self.config["api_key"],
+            location="header"
+        )
+
 
     def get_next_page_token(
         self, response: requests.Response, previous_token: Optional[Any]
